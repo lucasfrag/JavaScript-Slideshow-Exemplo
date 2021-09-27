1dec3092201e1afc716325ade1385f3cf7f6781a
Lucas Fraga <ti.lucasfraga@gmail.com>
2021-09-27 20:36:26 -0300

    Slides automatizados

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex2 = 0;
+//showSlides(slideIndex);
+showSlidesAutomatic();
 
 // Controle de Próximo/Anterior
 function plusSlides(n) {
@@ -16,14 +18,33 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlidesAutomatic() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) { slideIndex2 = 1 }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+  setTimeout(showSlidesAutomatic, 2000);
 }
